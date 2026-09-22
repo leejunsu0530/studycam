d2ad4e87056088fb37bdc9ae9f0827ddf3f05697
leejunsu <leejunsu0530@gmail.com>
2026-09-22 20:59:18 +0900

    카메라 클래스 뼈대

--- studycam/cam.py
+++ studycam/cam.py
@@ -1,6 +1,24 @@
+import sys
+import os
 from pathlib import Path
 import cv2
 import rich
 from ._console import console
-import sys
-import os
+
+
+class Camera:
+    def __init__(
+        self,
+        photo_path: str | Path
+    ) -> None:
+        """
+        - 카메라 루프에 대한 제어
+        - 찍은 사진에 대한 제어 및 관리
+        - 스레드 처리 주의
+
+        - 카메라만 다루는 만큼 전체적인 파일들의 경로는 여기서 관리하지 않음.
+        """
+        self.photo_path = Path(photo_path)
+
+    def save_photo(self):
+        pass
